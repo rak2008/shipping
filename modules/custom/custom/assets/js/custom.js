8fe59134fa5ad5e7008dac70271c7ae107f55c8a
Mohammed Gomma <M.Gomma-c@tamkeentech.sa>
2016-10-27 23:10:38 +0300

    custom module with assets for goelocation current loc

---
+++ modules/custom/custom/assets/js/custom.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function($){
+    $(document).ready(function(){
+        setTimeout(function(){$('.geocode-controls-wrapper .locate').click()}, 5000);
+    })
+})(jQuery)
